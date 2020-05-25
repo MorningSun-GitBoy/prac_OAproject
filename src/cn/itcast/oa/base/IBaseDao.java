e92a24b7ca73d82336a6d18120d69beb12fd69d5
Bachelor <AndyWolfCF2013@163.com>
2020-05-25 16:03:34 +0800

    create general dao and the package tree

---
+++ src/cn/itcast/oa/base/IBaseDao.java
@@ -0,0 +1,16 @@
+package cn.itcast.oa.base;
+
+import java.util.List;
+/**
+ * ps:通用Dao接口
+ * @author Bachelor
+ * @param <T>
+ */
+public interface IBaseDao<T> {
+	public void save(T entity);//添加 ：add
+	public void delete(Long id);//根据id删除 ： delete by id
+	public void update(T entity);//根据id修改 ：update by id
+	public T getById(Long Id);//根据id查询 ：select by id
+	public List<T> getByIds(Long[] ids);//根据id查询多个 ：multiply select by ids
+	public List<T> findAll();//输出整表 ：print the whole table
+}
