8cb735e5f43fced06810ec526e6e6219c1974c5e
Bachelor <AndyWolfCF2013@163.com>
2020-06-01 21:56:08 +0800

    build entity in domain

---
+++ src/cn/itcast/oa/domain/User.java
@@ -0,0 +1,108 @@
+package cn.itcast.oa.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * ps:用户实体
+ * 
+ * @author Bachelor
+ *
+ */
+public class User {
+	private long id;
+	private String loginName;
+	private String name;
+	private int gender;
+	private String phone;
+	private String email;
+	private String description;
+	private String password;
+	private Set<Department> departments;
+	private Set<Role> roles;
+
+	public User() {
+		this.departments = new HashSet<Department>();
+		this.roles = new HashSet<Role>();
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getGender() {
+		return gender;
+	}
+
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Department> getDepartments() {
+		return departments;
+	}
+
+	public void setDepartments(Set<Department> departments) {
+		this.departments = departments;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+}
