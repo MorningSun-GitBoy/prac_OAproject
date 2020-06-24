65924bb674f9f18851b535f8b3a3a2ce9fdac85c
Bachelor <AndyWolfCF2013@163.com>
2020-06-24 17:40:52 +0800

    user management

--- src/cn/itcast/oa/action/UserAction.java
+++ src/cn/itcast/oa/action/UserAction.java
@@ -9,5 +9,32 @@ import cn.itcast.oa.domain.User;
 @Controller
 @Scope("prototype")
 public class UserAction extends BaseAction<User>{
-
+	
+	public String list() {
+		return "list";
+	}
+	
+	public String delete() {
+		return "tolist";
+	}
+	
+	public String addUI() {
+		return "addUI";
+	}
+	
+	public String add() {
+		return "tolist";
+	}
+	
+	public String editUI() {
+		return "editUI";
+	}
+	
+	public String edit() {
+		return "edit";
+	}
+	
+	public String initPW() {
+		return "tolist";
+	}
 }
