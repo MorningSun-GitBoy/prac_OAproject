93278011f6c6023d0e437a9deef36d5abe43a15c
Bachelor <AndyWolfCF2013@163.com>
2020-07-27 22:51:52 +0800

    new service

---
+++ src/cn/itcast/oa/dao/IPrivilegeDao.java
@@ -0,0 +1,8 @@
+package cn.itcast.oa.dao;
+
+import cn.itcast.oa.base.IBaseDao;
+import cn.itcast.oa.domain.Privilege;
+
+public interface IPrivilegeDao extends IBaseDao<Privilege> {
+
+}
---
+++ src/cn/itcast/oa/service/impl/PrivilegeServiceImpl.java
@@ -0,0 +1,7 @@
+package cn.itcast.oa.service.impl;
+
+import cn.itcast.oa.service.IPrivilegeService;
+
+public class PrivilegeServiceImpl implements IPrivilegeService {
+
+}
